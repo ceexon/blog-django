ccd5723d88e98cc6fd22887f4edd30bacfbbc3c7
zonecc <bkevery@gmail.com>
2019-05-14 07:39:05 +0300

    removed user aspect

--- blog/apps/blogs/models.py
+++ blog/apps/blogs/models.py
@@ -1,18 +1,15 @@
 from django.db import models
-from ..users.models import User
+from ... import settings
+User = settings.AUTH_USER_MODEL
 
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     updated_at = models.DateTimeField(default=None, blank=True, null=True)
     deleted_at = models.DateTimeField(default=None, blank=True, null=True)
 
     def __str__(self):
         return self.title
-
-    def poster(self):
-        poster = User.objects.get(id=self.user)
-        return poster.first_name+' '+poster.other_names
