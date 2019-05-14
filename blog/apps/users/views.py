83f4dd3897e50e2154cba939b8e839378a4b45c2
zonecc <bkevery@gmail.com>
2019-05-14 07:44:29 +0300

    login view and new user view

--- blog/apps/users/views.py
+++ blog/apps/users/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
-from rest_framework import generics, response
-from .serializers import UserSerializer
+from rest_framework import generics, response, viewsets
+from rest_framework.views import APIView
+from .serializers import ProfileSerializer, LoginSerializer, UserSerializer
 from ..blogs.models import BlogPost
 from ..blogs.serializers import BlogPostSerializer
-from .models import User
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .models import User, Profile
 
 
 class CreateUser(generics.ListCreateAPIView):
+    permission_classes = (AllowAny,)
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class LoginUser(APIView):
+    permission_classes = (AllowAny,)
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        print('\n\n\n', request.data)
+        serializer.is_valid(raise_exception=True)
+        print('\n\n\n', serializer.data, '\n\n\n')
+
+        return response.Response(serializer.data)
 
 
 class ListUser(generics.ListAPIView):
@@ -28,7 +45,7 @@ class UserPosts(generics.ListAPIView):
     lookup_field = "id"
 
     def list(self, request, id, *args, **kwargs):
-        posts = BlogPost.objects.filter(user=id)
+        posts = BlogPost.objects.filter(author=id)
 
         serializer = self.serializer_class(posts, many=True)
         return response.Response(serializer.data)
