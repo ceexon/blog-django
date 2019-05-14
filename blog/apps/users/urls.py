17bbc9e6176b2c3ee8ccf8dfddc9b65895b88457
zonecc <bkevery@gmail.com>
2019-05-14 07:44:00 +0300

    user login urls

--- blog/apps/users/urls.py
+++ blog/apps/users/urls.py
@@ -1,4 +1,4 @@
-from .views import CreateUser, RUDUser, ListUser, UserPosts
+from .views import CreateUser, RUDUser, ListUser, UserPosts, LoginUser
 from django.urls import path, include
 from rest_framework.urls import url
 
@@ -7,6 +7,6 @@ urlpatterns = [
     path('new/', CreateUser.as_view(), name='new-user'),
     url(r'^(?P<id>[0-9]+)/$', RUDUser.as_view(), name='user-detail'),
     path('', ListUser.as_view(), name='all-users'),
-    # url(r'^posts/$', UserPosts.as_view(), name='user-posts'),h
+    url(r'^login/?$', LoginUser.as_view()),
     url(r'^posts/(?P<id>[0-9]+)/$', UserPosts.as_view(), name='user-posts'),
 ]
