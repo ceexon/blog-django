e4be4348aa066be0350954328c48b39d7ead0f36
zonecc <bkevery@gmail.com>
2019-05-16 14:30:19 +0300

    added image and caption fields

--- blog/apps/blogs/models.py
+++ blog/apps/blogs/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from ... import settings
+from django.core.validators import URLValidator
+
 User = settings.AUTH_USER_MODEL
 
 
 class BlogPost(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=150)
     content = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
+    image = models.TextField(max_length=1000, validators=[
+                             URLValidator], null=True, blank=True)
+    caption = models.CharField(max_length=150, blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
     updated_at = models.DateTimeField(default=None, blank=True, null=True)
     deleted_at = models.DateTimeField(default=None, blank=True, null=True)
 
