e53e43b7ad680bf2233b3ca38939e464a5bb203f
zonecc <bkevery@gmail.com>
2019-05-19 05:15:45 +0300

    updated user tests

--- blog/apps/users/tests.py
+++ blog/apps/users/tests.py
@@ -1,10 +1,12 @@
 from rest_framework.test import APITestCase, APIClient
+from django.test import TestCase, TransactionTestCase
 from rest_framework.views import status
 import json
 
 
-class BaseTest(APITestCase):
+class BaseTest(TransactionTestCase):
     def setUp(self):
+        self.client = APIClient()
         new_user = {
             "email": "trevor@zone.code",
             "password": "zonecode123",
@@ -17,8 +19,29 @@ class BaseTest(APITestCase):
             }
         }
 
+        new_user_1 = {
+            "email": "trevors@zone.code",
+            "password": "zonecode123",
+            "profile": {
+                "first_name": "Zee",
+                "other_names": "Zonecc",
+                "year_of_birth": "1998-07-03 08:00",
+                "bio": "Django Training developer upcoming",
+                "gender": 1
+            }
+        }
+
+        self.new_post = {
+            'title': 'Post title',
+            'content': 'This is the post content',
+            'caption': 'no image for this post',
+        }
+
         new_user_response = self.client.post(
             "/users/new/", new_user, format="json")
+
+        new_user_1_response = self.client.post(
+            "/users/new/", new_user_1, format="json")
 
 
 class UserTests(BaseTest):
@@ -43,7 +66,39 @@ class UserTests(BaseTest):
             format='json'
         )
 
+        response1 = self.client.post(
+            '/users/new/',
+            {
+                "email": "trevzee@zonecc.com",
+                "password": "zonec124",
+                "profile": {
+                    "first_name": "Zee",
+                    "other_names": "Zonecc",
+                    "year_of_birth": "1998-07-03 08:00",
+                    "bio": "Django Training developer upcoming",
+                    "gender": 1
+                }
+            },
+            format='json'
+        )
+
+        self.assertEqual(response1.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_add_new_user_fail(self):
+        """
+        Test when a requred field is missing
+        """
+
+        response = self.client.post(
+            '/users/new/',
+            {
+                "email": "trevzee@zonecc.com",
+            },
+            format='json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_user_login(self):
         response = self.client.post(
@@ -56,6 +111,26 @@ class UserTests(BaseTest):
         )
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_login_missing_email_password(self):
+        response = self.client.post(
+            '/users/login/',
+            {
+                "password": "zonecode123",
+            },
+            format='json'
+        )
+
+        response1 = self.client.post(
+            '/users/login/',
+            {
+                "email": "trevor@zone.code",
+            },
+            format='json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response1.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_invalid_login_credentions(self):
         wrong_email = {
