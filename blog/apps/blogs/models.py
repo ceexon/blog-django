155b5023d17f858c466440dc9519b5a577b84f32
zonecc <bkevery@gmail.com>
2019-05-19 18:55:59 +0300

    made image a url field

--- blog/apps/blogs/models.py
+++ blog/apps/blogs/models.py
@@ -11,8 +11,8 @@ class BlogPost(models.Model):
     slug = models.SlugField(unique=True, max_length=200, blank=True, null=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, blank=True, null=True)
-    image = models.TextField(max_length=1000, validators=[
-                             URLValidator], null=True, blank=True)
+    image = models.URLField(max_length=1000, validators=[
+        URLValidator], null=True, blank=True)
     caption = models.CharField(max_length=150, blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(default=None, blank=True, null=True)
