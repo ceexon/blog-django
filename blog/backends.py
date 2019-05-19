a7311422f527ede50121804bb317f75490a25f7a
zonecc <bkevery@gmail.com>
2019-05-19 05:16:18 +0300

    updated getting query

--- blog/backends.py
+++ blog/backends.py
@@ -2,6 +2,7 @@ import jwt
 from . import settings
 from .apps.users.models import User
 from .apps.blogs.models import BlogPost
+from django.shortcuts import get_object_or_404
 
 
 def decode_token(token):
@@ -13,7 +14,7 @@ def decode_token(token):
 
 def is_owner(token, post_id):
     user_id = decode_token(token)
-    post = BlogPost.objects.get(id=post_id)
+    post = get_object_or_404(BlogPost, id=post_id)
     author = post.author.id
 
     if user_id == author:
