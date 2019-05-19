439048008a88ec61bbbf29440aa6c52c592bca4f
zonecc <bkevery@gmail.com>
2019-05-19 20:48:01 +0300

    image

--- blog/apps/blogs/serializers.py
+++ blog/apps/blogs/serializers.py
@@ -6,4 +6,4 @@ class BlogPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogPost
         fields = ['id', 'slug', 'title', 'content',
-                  'date', 'caption', 'author', "image_url"]
+                  'date', 'caption', 'author', "image_url", "image"]
